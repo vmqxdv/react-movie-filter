0a8697b3812a803429549cee0796e7c44450ce57
vmqxdv <vquintilio@gmail.com>
2025-04-10 17:04:20 +0200

    feat: Mostrata la lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+import movieData from './data/movieData.json';
+
 function App() {
+
+  const [movieList, setMovieList] = useState(movieData)
+
+  const renderMovieList = (list) => {
+    return list.map((movie, i) => 
+      <tr key={i}>
+        <td>{movie.title}</td>
+        <td>{movie.genre}</td>
+      </tr>
+    )
+  };
+
   return (
-    <>
-    </>
+    <main>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Movie Title</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderMovieList(movieList)}
+          </tbody>
+        </table>
+      </div>
+    </main>
   )
 }
 
